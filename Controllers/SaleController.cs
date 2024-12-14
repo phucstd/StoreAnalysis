28ac267f65818cf695fca3fbbe448a88c7e8325d
phuc.nor <phuc.nor@pentoro.io>
2024-12-15 01:44:55 +0700

    + add statistic

--- Controllers/SaleController.cs
+++ Controllers/SaleController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StoreAnalysis.Data;
+using StoreAnalysis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StoreAnalysis.Controllers
 {
@@ -15,6 +19,81 @@ namespace StoreAnalysis.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public ActionResult RevenueChart()
+        {
+            var data = _context.Sales
+                        .Join(
+                            _context.ItemsStorage,  // Bảng cần join
+                            sale => sale.ItemId,    // Khóa ngoại trong bảng Sales
+                            item => item.Id,        // Khóa chính trong bảng ItemsStorage
+                            (sale, item) => new     // Kết quả join
+                            {
+                                SaleDate = sale.SaleDate,
+                                Price = item.Price
+                            }
+                        )
+                        .GroupBy(x => x.SaleDate.Month) // Nhóm theo tháng
+                        .Select(g => new
+                        {
+                            Month = g.Key,
+                            Revenue = g.Sum(x => x.Price) // Tổng doanh thu của tháng
+                        })
+                        .OrderBy(x => x.Month)
+                        .ToList();
+
+
+            var model = new RevenueChartViewModel
+            {
+                Labels = data.Select(x => $"Month {x.Month}").ToArray(),
+                Data = data.Select(x => x.Revenue).ToArray()
+            };
+
+            return Json(model);
+        }
+
+        // 2. Biểu đồ sản phẩm bán chạy
+        public ActionResult ProductSalesChart()
+        {
+            var data = _context.Sales
+                .GroupBy(s => s.ItemId)
+                .Select(g => new
+                {
+                    ItemName = _context.ItemsStorage.FirstOrDefault(i => i.Id == g.Key).ItemName,
+                    Quantity = g.Count()
+                })
+                .OrderByDescending(x => x.Quantity)
+                .ToList();
+
+            var model = new ProductSalesChartViewModel
+            {
+                ItemNames = data.Select(x => x.ItemName).ToArray(),
+                Quantities = data.Select(x => x.Quantity).ToArray()
+            };
+
+            return Json(model);
+        }
+
+        // 3. Biểu đồ bán hàng theo giờ
+        public ActionResult HourlySalesChart()
+        {
+            var data = _context.Sales
+                .GroupBy(s => s.SaleDate.Hour) // Nhóm theo giờ
+                .Select(g => new
+                {
+                    Hour = g.Key,
+                    Quantity = g.Count()
+                })
+                .OrderBy(x => x.Hour)
+                .ToList();
+
+            var model = new HourlySalesChartViewModel
+            {
+                Hours = data.Select(x => $"{x.Hour}:00").ToArray(),
+                Quantities = data.Select(x => x.Quantity).ToArray()
+            };
+
+            return Json(model);
         }
     }
 
---
+++ Models/Statistics.cs
@@ -0,0 +1,20 @@
+﻿namespace StoreAnalysis.Models
+{
+    public class RevenueChartViewModel
+    {
+        public string[] Labels { get; set; } // Ví dụ: Tháng, Ngày
+        public float[] Data { get; set; } // Doanh thu từng mục
+    }
+
+    public class ProductSalesChartViewModel
+    {
+        public string[] ItemNames { get; set; } // Tên sản phẩm
+        public int[] Quantities { get; set; }  // Số lượng bán
+    }
+
+    public class HourlySalesChartViewModel
+    {
+        public string[] Hours { get; set; } // Giờ (00:00, 01:00, ...)
+        public int[] Quantities { get; set; }  // Số lượng bán mỗi giờ
+    }
+}
